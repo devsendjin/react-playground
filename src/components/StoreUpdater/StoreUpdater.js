6cf54831270a5b7a63490b11b3048fc3d413ac7e
Anatoliy Skichko <anatoliy.skichko.dev@gmail.com>
2021-08-28 15:55:31 +0300

    StoreUpdater: fix issue with redux async update

--- src/components/StoreUpdater/StoreUpdater.js
+++ src/components/StoreUpdater/StoreUpdater.js
@@ -1,41 +1,26 @@
-import { useDispatch, useSelector } from "react-redux";
-import { updateListItemAsyncAction } from "../../store/Global/actions";
-import "bulma/bulma.sass";
+import {useDispatch, useSelector} from "react-redux";
+import {updateListItemAsyncAction} from "../../store/Global/actions";
+import 'bulma/bulma.sass';
 
-  export const StoreUpdater = () => {
-  const list = useSelector((store) => store.global.list);
+export const StoreUpdater = () => {
+  const list = useSelector(store => store.global.list);
   const dispatch = useDispatch();
 
   const onUpdate = (item) => {
-    dispatch(
-      updateListItemAsyncAction({ value: item.value, isLoading: true }, 0)
-    );
-    dispatch(
-      updateListItemAsyncAction({ value: item.value, isLoading: false }, 2000)
-    );
-  };
+     dispatch(updateListItemAsyncAction(item, 2000))
+  }
 
   return (
     <div className="store-updater container">
       <h1 className="title has-text-centered">StoreUpdater</h1>
       {list.length > 0 && (
         <div className="list columns is-multiline is-flex-wrap-wrap mt-6">
-          {list.map((item) => (
-            <div
-              className="card column is-4 m-0 has-text-centered"
-              key={item.value}
-            >
+          {list.map(item => (
+            <div className="card column is-4 m-0 has-text-centered" key={item.value}>
               <div className="content">{item.value}</div>
-              <div style={{ height: "25px" }}>
-                {item.isLoading && "Updating..."}
-              </div>
+              <div style={{height: '25px'}}>{item.isLoading && 'Updating...'}</div>
               <footer className="card-footer">
-                <button
-                  className="button card-footer-item"
-                  onClick={() => onUpdate(item)}
-                >
-                  Update
-                </button>
+                <button className="button card-footer-item" onClick={() => onUpdate(item)}>Update</button>
               </footer>
             </div>
           ))}
--- src/store/Global/actions.js
+++ src/store/Global/actions.js
@@ -1,30 +1,29 @@
-import { SET_LIST } from "./actionTypes";
+import {SET_LIST} from "./actionTypes";
 
 export const setListAction = (payload) => {
   return {
     type: SET_LIST,
-    payload
-  };
-};
+    payload,
+  }
+}
 
-export const updateListItemAsyncAction = (payload, timeout) => async (
-  dispatch,
-  getState
-) => {
-  const list = getState().global.list;
-  const listCopy = list.slice();
+export const updateListItemAsyncAction = (payload, timeout) => async (dispatch, getState) => {
 
-  const index = listCopy.findIndex((item) => item.value === payload.value);
-  listCopy.splice(index, 1, {
-    ...listCopy[index],
-    isLoading: payload.isLoading
-  });
+  const getUpdatedList = (list, _payload, isLoading) => {
+    return list.map(item => item.value === _payload.value ? {
+      ...item,
+      isLoading
+    } : item)
+  }
+
+  dispatch(setListAction(getUpdatedList(getState().global.list, payload, true)));
 
   await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, timeout);
-  }).then(() => {
-    dispatch(setListAction(listCopy));
-  });
-};
+      setTimeout(() => {
+        resolve();
+      }, timeout);
+    }
+  )
+
+  dispatch(setListAction(getUpdatedList(getState().global.list, payload, false)));
+}
